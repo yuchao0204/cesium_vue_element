9d900054a7ea43b9f8fb4ef10acf3b3c558fe2a7
yuchao0204 <314014799@qq.com>
2020-09-01 16:23:38 +0800

    v0

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="viewer">
+    <div id = 'cesiumContainer'>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+// import * as Cesium from 'cesium/Cesium';
+// import widgets from 'cesium/Widgets/widgets.css';
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted(){
+    // this.showtianditu();
+    // var viewer = new Cesium.Viewer('cesiumContainer');
+  },
+  methods:{
+    showtianditu(){
+      var viewer = new Cesium.Viewer("cesiumContainer", {
+        terrainProvider: Cesium.createWorldTerrain(),
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .viewer {
+    width: 100%;
+    height: 400px;
+  }
+</style>
